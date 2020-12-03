16d74708a2cfa16f96302215709e5264ac929ec3
timyi976 <b09902067@ntu.edu.tw>
2020-12-03 15:00:39 +0800

    edit annotations and comments

--- agent/guanboo-yang.py
+++ agent/guanboo-yang.py
@@ -7,13 +7,12 @@ class MyAgent(BaseAgent):
         super(MyAgent, self).__init__()
     
     def step(self, reward, obs):
-        def transfer(obsDic):
+        def transfer(obsDic:dict) -> dict:
             '''
             Transfer dictionary obsDic into a new dictionary which uses 2D position tuple as its keys instead of 1D index
 
-            Return: Dictionary
-                    a new dictionary with the modified key which in the form of (y,x)
-                    where y indicates the col (from 0 to 7) and x represents the row (from 0 to 7)
+            Return: Dictionary. A new dictionary with the modified key which in the form of (y,x)
+            where y indicates the col (from 0 to 7) and x represents the row (from 0 to 7)
             '''
             return {(i%self.cols_n,i//self.rows_n):obsDic[i] for i in obsDic}
         obsNew=transfer(obs)    #new dictionary with 2D postion tuple keys
