00b6f9bc34855c6a9599feee7b326ab65cf7e3f0
楊冠柏 <yangguanbo@gbymacbookpro.local>
2020-12-13 01:32:27 +0800

    update

--- agent/guanboo-yang.py
+++ agent/guanboo-yang.py
@@ -64,18 +64,17 @@ class MyAgent(BaseAgent):
         def isOnSide(move):
             return (move[0] in {0, self.cols_n-1} or move[1] in {0, self.rows_n-1}) and not isOnCorner(move)
         
+        # change position
         def right(move):
             return (move[0]+1, move[1])
-        
         def left(move):
             return (move[0]-1, move[1])
-        
         def up(move):
             return (move[0], move[1]+1)
-        
         def down(move):
             return (move[0], move[1]-1)
         
+        # Side move is good or bad
         def isGoodSideMove(move, color) -> bool:
             obsTemp = obsNew.copy()
             if not isOnSide(move): return False
@@ -282,7 +281,14 @@ class MyAgent(BaseAgent):
         
         stepNum = countSteps(obsNew)
         
-        if stepNum <= 52:
+        if stepNum == 5:
+            for move in [(2, 2), (5, 5)]:
+                if isValidMove(obsNew, move, colorNum):
+                    x, y = move
+            return (self.col_offset + x * self.block_len, self.row_offset + y * self.block_len), pygame.USEREVENT
+
+        
+        elif stepNum <= 51:
             # rondom choice
             keys = list(openRateDict(obsNew, colorNum).keys())
             random.shuffle(keys)
@@ -359,140 +365,3 @@ class RandomAgent(BaseAgent):
         x, y = possibleMoves[0]
         return (self.col_offset + x * self.block_len, self.row_offset + y * self.block_len), pygame.USEREVENT
 
-class MyAgent2(BaseAgent):
-    
-    def __init__(self, color = "black", rows_n = 8, cols_n = 8, width = 600, height = 600):
-        super().__init__(color, rows_n, cols_n, width, height)
-    
-    def step(self, reward:dict, obs:dict) -> tuple:
-        colorDict = {"black": -1, "white": 1, "empty": 0}
-        colorNum = colorDict[self.color]
-        
-        def transfer(obsDict:dict) -> dict:
-            '''
-            obsDict: dict
-                key: 0 ~ 63
-                val: [-1, 0, 1] (black, empty, white)
-            
-            return : dict
-                key: (x, y), where (7, 0) represents the top right
-                val: [-1, 0, 1] (black, empty, white)
-            '''
-            return {(i % self.cols_n, i // self.cols_n):obsDict[i] for i in obsDict}
-        
-        obsNew=transfer(obs)    # new dictionary with 2D postion tuple keys
-        
-        def isOnBoard(x, y) -> bool:
-            return 0 <= x < self.cols_n and 0 <= y < self.rows_n
-        
-        def isValidMove(obs:dict, move: tuple, color=colorNum) -> list:
-            if not isOnBoard(move[0], move[1]) or obs[(move[0], move[1])] != 0:
-                return []
-            obs[(move[0], move[1])] = color
-            dirs = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-            tilesToFlip = []
-            for xdir, ydir in dirs:
-                x, y = move[0]+xdir, move[1]+ydir
-                while isOnBoard(x, y) and obs[(x, y)] == -color:
-                    x += xdir; y += ydir
-                    if isOnBoard(x, y) and obs[(x, y)] == color:
-                        while True:
-                            x -= xdir; y -= ydir
-                            if x == move[0] and y == move[1]:
-                                break
-                            tilesToFlip.append((x, y))
-            obs[(move[0], move[1])] = 0
-            return tilesToFlip
-        
-        # Corner position
-        def isOnCorner(move):
-            return move[0] in {0, self.cols_n-1} and move[1] in {0, self.rows_n-1}
-        
-        # Side position
-        def isOnSide(move):
-            return move[0] in {0, self.cols_n-1} or move[1] in {0, self.rows_n-1}
-        
-        # X position
-        def isBadMove(move):
-            return move[0] in {1, self.cols_n-2} and move[1] in {1, self.rows_n-2}
-        
-        def getValidMovesDict(obs, color=colorNum) -> dict:
-            return {(x, y):isValidMove(obs, (x, y), color) for x in range(self.cols_n) for y in range(self.rows_n) if isValidMove(obs, (x, y), color)}
-        
-        def countOpenRate(flip:tuple, obs:dict) -> int:
-            count = 0
-            dirs = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-            for xdir, ydir in dirs:
-                x, y = flip[0]+xdir, flip[1]+ydir
-                if isOnBoard(x, y) and obs[(x, y)] == 0:
-                    count += 1
-            return count
-        
-        def openRateDict(obs) -> dict:
-            validMovesDict = getValidMovesDict(obs)
-            openRateDict = {}
-            
-            # try remove bad move
-            for movek, movev in validMovesDict.copy().items():
-                
-                # don't flip X position
-                for flip in movev:
-                    if isBadMove(flip):
-                        validMovesDict.pop(movek, None)
-                        if validMovesDict == {}:
-                            validMovesDict[movek] = movev
-                
-                # don't place X position
-                if isBadMove(movek):
-                    validMovesDict.pop(movek, None)
-                    if validMovesDict == {}:
-                        validMovesDict[movek] = movev
-                
-                # don't let the opponent play good move
-                obsTest = obsNew.copy()
-                opponentMovesbef = getValidMovesDict(obsTest, -colorNum)
-                obsTest[(movek)] = colorNum
-                opponentMovesaft = getValidMovesDict(obsTest, -colorNum)
-                for move in [i for i in opponentMovesaft if i not in opponentMovesbef]:
-                    if isOnCorner(move):
-                        validMovesDict.pop(movek, None)
-                        if validMovesDict == {}:
-                            validMovesDict[movek] = movev
-            
-            for move in validMovesDict:
-                count = 0
-                for flip in validMovesDict[move]:
-                    count += countOpenRate(flip, obs)
-                openRateDict[move] = count
-            
-            return openRateDict
-        
-        def hereIsPriority(obs):
-            possibleMoves = list(openRateDict(obs).keys())
-            # Corner position first
-            for move in possibleMoves:
-                if isOnCorner(move):
-                    return move
-            
-            # Side position next
-            for move in possibleMoves:
-                if isOnSide(move):
-                    return move
-            
-            return False
-        
-        # rondom choice
-        keys = list(openRateDict(obsNew).keys())
-        random.shuffle(keys)
-        randomDict = {key:openRateDict(obsNew)[key] for key in keys}
-        
-        # sorted with openrate
-        sortedOpenRateDict = {k:v for k, v in sorted(randomDict.items(), key=lambda x: x[1])}
-        try: x, y = next(iter(sortedOpenRateDict))
-        except StopIteration: return
-        
-        # priority move
-        if hereIsPriority(obsNew):
-            x, y = hereIsPriority(obsNew)
-        
-        return (self.col_offset + x * self.block_len, self.row_offset + y * self.block_len), pygame.USEREVENT
